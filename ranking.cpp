72487ba57559e767e326aebcaac9433843ec1eee
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2025-09-27 23:38:29 +0200

    Remove magic numbers and obsolete include path

--- ranking.cpp
+++ ranking.cpp
@@ -2,9 +2,14 @@
 #include "ranking.hpp"
 
 void show_ranking() {
+
+    // The column and row to print the warning to.
+    constexpr int WARNING_COL = 2;
+    constexpr int WARNING_ROW = 2;
+    
     if (!fs::exists("data.json")) {
         WINDOW * no_ranking = new_boxed_window(POPUP_HEIGHT, POPUP_WIDTH);
-        mvwprintw(no_ranking, 2, 2, "No ranking data");
+        mvwprintw(no_ranking, WARNING_ROW, WARNING_COL, "No ranking data");
         wgetch(no_ranking);
     }
 
