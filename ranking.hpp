7ce7153a10d0a5575ee57b77f305a3e98181d1af
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2025-09-28 16:46:47 +0200

    Add helper function to show error if data.json is not available

--- ranking.hpp
+++ ranking.hpp
@@ -6,6 +6,11 @@ using std::string;
 #include "json.hpp"
 using json = nlohmann::json;
 
+// Creates a window that shows the data in data.json.
 void show_ranking();
 
+// Creates a pop-up window that shows the error string.
+void show_error(string error);
+
+// Updates the data.json file to add a new clear time.
 void update_ranking(string current_player, int time);
