c3435b606d33559615b59643769e823b897d078a
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2025-09-28 16:48:03 +0200

    Update ask_username() to remove trailing placeholders

--- username.cpp
+++ username.cpp
@@ -2,9 +2,10 @@
 #include "username.hpp"
 
 string ask_username() {
+    
     WINDOW * username_window = new_boxed_window(POPUP_HEIGHT, POPUP_WIDTH);
 
-    string dialogue = "Enter username";
+    string dialogue     = "Enter username";
     string placeholders = "______________";
 
     mvwprintw(username_window, 1, 3, dialogue.data());
@@ -18,9 +19,12 @@ string ask_username() {
     string new_username(MAX_USERNAME_LENGTH, '\0');
     if (mvwgetnstr(username_window, 3, 3, new_username.data(), new_username.size()) == ERR) 
         new_username.clear();
+    
+    std::erase(new_username, '_');
     new_username.resize((new_username.length()));
 
-    noecho(); curs_set(0); delwin(username_window);
+    noecho(); curs_set(0);
+    delwin(username_window);
 
     return new_username;
 }
