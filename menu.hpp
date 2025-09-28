b67a6d6fd4be23c328a6d6db579eae3f42be9511
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2025-09-28 16:44:32 +0200

    Move constants into implementation file; refactor redundant code

--- menu.hpp
+++ menu.hpp
@@ -3,15 +3,14 @@
 #include "curses.h"
 using std::string;
 
-/**
- *  Wrapper function that contains menu logic.
- */
+// Wrapper function that contains menu logic.
 void menu(string current_player);
 
 /**
  * Function to select one of the options on the menu.
  * 
- * @param current_selection     The current selected option on the menu.
+ * @param current_selection     The index of the current selected
+ *                              option in the anchors[] array.
  * @param menu_window           Pointer to the menu window.
  * @return                      Index of the current selected option in 
  *                              the anchors[] array in visualize_menu().
@@ -21,7 +20,8 @@ int menu_selection(int current_selection, WINDOW * menu_window);
 /**
  * Function to visualize the menu on the terminal.
  * 
- * @param current_selection     The current selected option on the menu.
+ * @param current_selection     The index of the current selected
+ *                              option in the anchors[] array.
  * @param menu_window           Pointer to the menu window.
  */
 void visualize_menu(int current_selection, WINDOW * menu_window);
