791406329371dc76ffd7dba61181aa24523d98f3
salvatorebruzzese <salvatorebruzzese05@gmail.com>
2025-10-02 17:31:01 +0200

    show_ranking() working draft

--- src/ranking.cpp
+++ src/ranking.cpp
@@ -3,6 +3,10 @@
 
 constexpr int WARNING_ROW = 2;
 constexpr int WARNING_COL = 2;
+constexpr int RANKING_HEIGHT = 8;
+constexpr int RANKING_WIDTH = 20;
+constexpr int PRINT_COL = 3;
+constexpr int MAX_PLAYERS = 5;
 
 void show_ranking() {
     
@@ -12,10 +16,39 @@ void show_ranking() {
     }
 
     std::ifstream f("data.json");
+
+    if (!json::accept(f)) {
+        f.close();
+        return;
+    }
+
+    f.clear();
+    f.seekg(0, std::ios::beg);
     
     json ranking = json::parse(f);
 
+    WINDOW * ranking_window = new_boxed_window(RANKING_HEIGHT, RANKING_WIDTH);
+    
+    mvwprintw(ranking_window, 1, 1, "Top 5");
+
+    int PRINT_ROW = 2;
+    int cycle = 0;
+
+    for (json::iterator it = ranking.begin();
+         it != ranking.end() && cycle < MAX_PLAYERS;
+         it++, PRINT_ROW++, cycle++) {
+        int score = it->at("score").get<int>();
+        string player = it->at("player").get<string>();
+
+        mvwprintw(ranking_window, PRINT_ROW, PRINT_COL, "%s: %d", player.c_str(), score);
+    }
+
+    wrefresh(ranking_window);
+
+    wgetch(ranking_window);
+
     f.close();
+    delwin(ranking_window);
 }
 
 void update_ranking(string current_player, int time) {
