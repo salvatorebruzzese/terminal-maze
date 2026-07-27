48053570c88c485a5d9b15322ba5676268e047e9
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2026-07-27 11:38:30 +0200

    fix: error in wall generation

--- src/maze.cpp
+++ src/maze.cpp
@@ -4,14 +4,12 @@
 
 void gridify(WINDOW* game_window) {
 
-    for (int starting_y = 1; starting_y <= GAME_HEIGHT_NO_BORDERS;
-         starting_y++) {
-        for (int starting_x = 1; starting_x <= GAME_WIDTH_NO_BORDERS;
-             starting_x++) {
-            if (starting_y % 2 == 0 && starting_x % 2 == 1) {
-                mvwadd_wch(game_window, starting_y, starting_x, WACS_BLOCK);
-            } else if (starting_y % 2 == 1 && starting_x % 2 == 0)
-                mvwadd_wch(game_window, starting_y, starting_x, WACS_BLOCK);
+    for (int y = 1; y <= GAME_HEIGHT_NO_BORDERS; y++) {
+        for (int x = 1; x <= GAME_WIDTH_NO_BORDERS; x++) {
+            if (y % 2 == 1 && x % 2 == 0) {
+                mvwadd_wch(game_window, y, x, WACS_BLOCK);
+            } else if (y % 2 == 0)
+                mvwadd_wch(game_window, y, x, WACS_BLOCK);
         }
         wrefresh(game_window);
         napms(20);
